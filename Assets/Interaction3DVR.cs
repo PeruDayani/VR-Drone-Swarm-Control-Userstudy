3bd8e388226fd4e4469b074366bf3228af264af0
PeruDayani <perudayani@berkeley.edu>
2020-02-13 13:26:06 -0800

    3d rotate not working

---
+++ Assets/Interaction3DVR.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.UnityEventHelper;
+using VRTK;
+
+public class Interaction3DVR : MonoBehaviour
+{
+    private GameObject curDrone;
+    private GameObject prevDrone;
+    public GameObject controllerRight;
+
+    // Bit shift the index of the layer (8) to get a bit mask
+    private int layerMask = 1 << 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerRight = GameObject.FindGameObjectWithTag("controller_right");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        RaycastHit destinationHit = controllerRight.GetComponent<VRTK_StraightPointerRenderer>().GetDestinationHit();
+        if (controllerRight.GetComponent<VRTK_Pointer>().IsStateValid() && destinationHit.collider != null)
+        {
+            if (destinationHit.collider.gameObject.name.Contains("warningSphere"))
+            {
+                //Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                curDrone = destinationHit.collider.gameObject.transform.parent.gameObject;
+                Debug.Log(curDrone);
+                bool bDown = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
+                Debug.Log(bDown);
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    Debug.Log("Clicked!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    /**
+                    if (curDrone.GetComponent<DroneProperties>().classPointer.safetyStatus == Drone.SafetyStatus.NOT_SAFE) // 0: not safe zone
+                    {
+                        curDrone.GetComponent<DroneProperties>().classPointer.RaiseDrone();
+                    }
+                    else if (curDrone.GetComponent<DroneProperties>().classPointer.safetyStatus == Drone.SafetyStatus.SAFE) // 2: safe zone
+                    {
+                        curDrone.GetComponent<DroneProperties>().classPointer.LowerDrone();
+                    }
+                    else
+                    {
+                        Debug.Log("Current status: " + curDrone.GetComponent<DroneProperties>().classPointer.safetyStatus);
+                    }
+                    **/
+                }
+
+                if (prevDrone == null)
+                {
+                    prevDrone = curDrone;
+                }
+
+                if (curDrone != prevDrone)
+                {
+                    // set previous drone game object to original
+                    //prevDrone.GetComponent<DroneProperties>().classPointer.DroneCollideRender(false);
+                    prevDrone = curDrone;
+                }
+                //curDrone.GetComponent<DroneProperties>().classPointer.DroneCollideRender(true);
+            }
+            
+        }
+        else
+        {
+            /**
+            if (curDrone != null)
+                curDrone.GetComponent<DroneProperties>().classPointer.DroneCollideRender(false);
+            **/
+        }
+    }
+}
---
+++ Assets/Navigate3DVR.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigate3DVR : MonoBehaviour
+{
+    //pivot is the center of the scene
+    public GameObject pivot;
+    //originalScale is the original localScale of the world
+    public Vector3 originalScale;
+    //This is the 1/10th of the originalScale of the world
+    public Vector3 minScale;
+    //This is the 10 times the originalScale of the world
+    public Vector3 maxScale;
+    //currentScale is the current localScale
+    public Vector3 currentScale;
+
+    // current world
+    public GameObject rotatingTable;
+
+    // Rotation
+    public LinkedList<float> angles;
+    public bool handleHeldTrigger = false;
+    public OVRInput.Controller currentController;
+    private Vector3 oldVec;
+    private float movementAngle;
+    public float movementAngleDecay = .95f;
+    private float rotSpeed = 0.05f;  // Rotation speed(in rev/s)
+    private float scalingSpeed = 0.05f;
+    //This is the speed at which the map can be moved at
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Pivot assignment
+        pivot = GameObject.FindWithTag("pivot");
+        originalScale = transform.localScale;
+        minScale = Vector3.Scale(originalScale, new Vector3(0.1F, 0.1F, 0.1F));
+        maxScale = Vector3.Scale(originalScale, new Vector3(10F, 10F, 10F));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+        ControllerRotateWorld();
+    }
+
+    private void MoveCamera()
+    {
+        // Control lateral movement
+        float moveX = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+        float moveZ = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y;
+
+        // Control height
+        float moveY = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).y;
+
+        Vector3 position = transform.position;
+        if (moveX != 0 || moveZ != 0)
+        {
+            // update map position based on input
+            position.x += moveX * speed * Time.deltaTime;
+            position.z += moveZ * speed * Time.deltaTime;
+            transform.position = position;
+        }
+
+        if (moveY != 0)
+        {
+            position.y += moveY * speed * Time.deltaTime;
+            transform.position = position;
+        }
+    }
+
+
+    private void ControllerRotateWorld()
+    {
+        float deltaX = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+
+        // We only consider inputs above a certain threshold.
+        if (Mathf.Abs(deltaX) > 0.2f)
+        {
+
+            float angle = deltaX * rotSpeed * 360 * Time.fixedDeltaTime;
+
+            transform.RotateAround(pivot.transform.position, Vector3.up, angle);
+
+            if (rotatingTable)
+            {
+                rotatingTable.transform.RotateAround(pivot.transform.position, Vector3.up, angle);
+            }
+
+        }
+    }
+}
